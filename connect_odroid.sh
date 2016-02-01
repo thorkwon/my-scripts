e9c1d4dc2395faaede3975fb124481c71c234f12
hoegeun kwon <hoegeun.kwon@samsung.com>
2016-02-01 23:48:56 +0900

    Modified name
    Signed-off-by: hoegeun kwon <hoegeun.kwon@samsung.com>

---
+++ connect_odroid.sh
@@ -0,0 +1,3 @@
+#bin/bash
+
+sudo mount -t cifs -o user='khg',password='gkfajsl' //192.168.0.10/Share /mnt/odroid
--- connet-odroid.sh
+++
@@ -1,3 +0,0 @@
-#bin/bash
-
-sudo mount -t cifs -o user='khg',password='gkfajsl' //192.168.0.10/Share /mnt/hdd
