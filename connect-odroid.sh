8dbceaeaeeae52b8290698f04f370fbbf557e943
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2016-04-06 22:50:04 +0900

    connect-odroid.sh: modified script
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- connect-odroid.sh
+++ connect-odroid.sh
@@ -1,3 +1,19 @@
 #bin/bash
 
-sudo mount -t cifs -o user='khg',password='passwork' //192.168.0.10/Share /mnt/odroid
+## check the user id
+if ! [ -n "$1" ] ; then
+	echo "set user id"
+	exit
+fi
+
+## umount odroid samba
+CMD=$(sudo mount | grep odroid)
+if [ -n "$CMD" ] ; then
+	echo "Un Mount odroid"
+	sudo umount /mnt/odroid
+fi
+
+echo "set user password:"
+read PWD
+sudo mount -t cifs -o user=$1,password=$PWD //192.168.0.10/Share /mnt/odroid
+echo "Mount odroid"
