ab15449cf3d51ded2452b637c3d64e12298e2bee
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2016-05-07 00:52:04 +0900

    ssh_odroid.sh: check the ttl size
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- ssh_odroid.sh
+++ ssh_odroid.sh
@@ -1,8 +1,8 @@
 #bin/bash
 
-MSG=$(ping -c 1 192.168.0.10 | grep errors)
+MSG=$(ping -W 1 -c 1 192.168.0.10 | grep ttl=64)
 
-if ! [ -n "$MSG" ]; then
+if [ -n "$MSG" ]; then
 	ssh odroid@192.168.0.10
 else
 	ssh -p 50022 odroid@khg.woobi.co.kr
