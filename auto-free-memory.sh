5bd95a793b207028bb12032330034edd584000c9
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2016-11-26 00:23:20 +0900

    auto-free-memory.sh: add auto free memory
    Auto free ram memory for Ubuntu 14.04
    
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ auto-free-memory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+free -h
+echo "_________________________________________________________________________"
+sudo sysctl -w vm.drop_caches=3
+sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+echo "_________________________________________________________________________"
+free -h
