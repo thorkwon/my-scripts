be63429bf9e80537d6715197999db35f8639abc1
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2016-12-29 22:13:08 +0900

    auto-free-memory.sh: add sync
    add sync before free memory.
    
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- auto-free-memory.sh
+++ auto-free-memory.sh
@@ -2,7 +2,9 @@
 
 free -h
 echo "_________________________________________________________________________"
+sudo sync
 sudo sysctl -w vm.drop_caches=3
-sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+sudo sync
+echo 3 | sudo tee /proc/sys/vm/drop_caches
 echo "_________________________________________________________________________"
 free -h
