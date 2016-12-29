a99a47c44b6df223c711670f5f80f1d93c554ff7
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2016-12-29 22:43:56 +0900

    auto-free-memory.sh: remove same drop cache
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- auto-free-memory.sh
+++ auto-free-memory.sh
@@ -5,6 +5,5 @@ echo "_________________________________________________________________________"
 sudo sync
 sudo sysctl -w vm.drop_caches=3
 sudo sync
-echo 3 | sudo tee /proc/sys/vm/drop_caches
 echo "_________________________________________________________________________"
 free -h
