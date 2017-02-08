b6043dfaf260d6e21d5a90a1bfa3df27decd9eae
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2017-02-08 23:09:58 +0900

    connect-server.sh: Fix the umount and check user id
    The first unmount, and Second check user id.
    
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- connect-server.sh
+++ connect-server.sh
@@ -1,17 +1,17 @@
 #bin/bash
 
-## check the user id
-if ! [ -n "$1" ] ; then
-	echo "set user id"
-	exit
-fi
-
 ## umount server samba
 CMD=$(sudo mount | grep server)
 if [ -n "$CMD" ] ; then
 	echo "un mount server"
 	sudo fuser -cu /mnt/server/
 	sudo umount /mnt/server
+fi
+
+## check the user id
+if ! [ -n "$1" ] ; then
+	echo "set user id"
+	exit
 fi
 
 echo "set user password:"
