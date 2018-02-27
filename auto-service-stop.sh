bc87fd69dc67718e330156ad1c04bc156366dc07
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2018-02-27 23:47:45 +0900

    auto-service-stop.sh: Add the script for server
    This script is required before rebooting the server.
    
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ auto-service-stop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# stop dlna server
+sudo service minidlna stop
+echo "Stop dlna server"
+
+# stop samba server
+sudo service smbd stop
+echo "Stop samba server"
+
+# stop torrent server
+TORRENT=$(ps -ef | grep qbit | grep nox | awk '{print $2}')
+sudo kill -9 $TORRENT
+echo "Stop torrent server"
+
+# umount hdd
+sudo fuser -cu /home/Share
+sudo umount /home/Share
+echo "Umount hdd"
