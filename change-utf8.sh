3eb3fd6d0afb0df5019c706d99bce5f52455655b
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2018-05-03 22:21:24 +0900

    change-utf8.sh: Add setting code for owner
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- change-utf8.sh
+++ change-utf8.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-CMD=$(file "$1" | grep UTF | wc -l)
+CMD=`file $1 | grep UTF | wc -l`
+USER=`ls -l $1 | awk '{print $3}'`
+GROUP=`ls -l $1 | awk '{print $4}'`
 
-if [ $CMD == "1" ] ; then
+if [ $CMD == 1 ]; then
 	echo "Already UTF-8"
 	exit 1
 fi
 
-touch "$1_"
+touch $1_
 
-iconv -c -f euc-kr -t utf-8 "$1" > "$1_"
+iconv -c -f euc-kr -t utf-8 $1 > $1_
 
-rm -f "$1"
-mv "$1_" "$1"
+rm -f $1
+mv $1_ $1
+
+sudo chown $USER:$GROUP $1
