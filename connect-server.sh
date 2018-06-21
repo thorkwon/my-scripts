417e94792a8f2284126d50e9165584ca6108c136
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2018-06-22 01:03:23 +0900

    connect-server.sh: Add owner permission option
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- connect-server.sh
+++ connect-server.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-## flag
 flag=0
 
-## umount server samba
-CMD=$(sudo mount | grep server)
+# umount samba server
+CMD=`sudo mount | grep server`
 if [ -n "$CMD" ] ; then
 	echo "Un mount server"
 	sudo fuser -cu /mnt/server/
@@ -12,16 +11,20 @@ if [ -n "$CMD" ] ; then
 	flag=1
 fi
 
-## check the user id
-if [ $flag = 1 -a -z "$1" ] ; then
+# check the user id
+if [ $flag = 1 -a -z "$1" ]; then
 	exit 1
-elif [ -z "$1" ] ; then
+elif [ -z "$1" ]; then
 	echo "set user id"
 	exit 1
 fi
 
-## You have to add hostname of server into '/etc/hosts'
-sudo mount -t cifs -o user=$1 //server/Share /mnt/server
+# get uid and gid
+U_ID=`id | awk '{print $1}' | awk -F '(' '{print $1}'`
+G_ID=`id | awk '{print $2}' | awk -F '(' '{print $1}'`
+
+# You have to add hostname of server into '/etc/hosts'
+sudo mount -t cifs -o user=$1,$U_ID,$G_ID //server/Share /mnt/server
 CMD=$(sudo mount | grep server)
 if [ -n "$CMD" ] ; then
 	echo "Mount server	user[$1]"
