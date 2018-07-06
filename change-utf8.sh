aa92c132ba2e2021a20bb9786f3a2485c78c16b8
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2018-07-07 00:18:32 +0900

    change-utf8.sh: Add support for string space of argument
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- change-utf8.sh
+++ change-utf8.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-CMD=`file $1 | grep UTF | wc -l`
-USER=`ls -l $1 | awk '{print $3}'`
-GROUP=`ls -l $1 | awk '{print $4}'`
-
-if [ $CMD == 1 ]; then
-	echo "Already UTF-8"
+if [ $# == 0 ]; then
+	echo "Usage: change-utf8.sh [filename]"
 	exit 1
 fi
 
-touch $1_
+# Backup env IFS and then set IFS to newline
+IFS_BACKUP="$IFS"
+IFS=$'\n'
 
-iconv -c -f euc-kr -t utf-8 $1 > $1_
+FILE_NAME=$1
 
-rm -f $1
-mv $1_ $1
+for token in $@; do
+	if [ "$token" == "$1" ]; then
+		continue
+	fi
+	FILE_NAME="$FILE_NAME"$' '"$token"
+done
 
-sudo chown $USER:$GROUP $1
+CMD=`file $FILE_NAME | grep UTF | wc -l`
+USER=`ls -l $FILE_NAME | awk '{print $3}'`
+GROUP=`ls -l $FILE_NAME | awk '{print $4}'`
+
+if [ $CMD == 1 ]; then
+	echo "Already UTF-8"$'\t'"[$FILE_NAME]"
+	# Restore IFS
+	IFS=$IFS_BACKUP
+	exit 1
+fi
+
+touch ${FILE_NAME}_
+
+iconv -c -f euc-kr -t utf-8 $FILE_NAME > ${FILE_NAME}_
+
+rm -f $FILE_NAME
+mv ${FILE_NAME}_ $FILE_NAME
+
+sudo chown $USER:$GROUP $FILE_NAME
+
+# Restore IFS
+IFS=$IFS_BACKUP
