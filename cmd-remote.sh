c4c2fcc3a8b4dd73db31ec8d9e35235fb1c83f22
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2018-11-20 21:55:45 +0900

    cmd-remote.sh: Add remote command script
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ cmd-remote.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+if ! [ -f "${SCRIPTPATH}/server.list" ]; then
+	echo "Cannot access 'server.list': No such file"
+	echo "You have to create a file 'server.list' in the '$SCRIPTPATH'"
+	exit 1
+fi
+
+if [ $# == 0 ]; then
+	echo "Usage: ./cmd-remote.sh [cmd]"
+	exit 1
+fi
+
+ROOT_PW=""
+if [ "$1" == "sudo" ]; then
+	echo "[sudo] password:"
+	read -s ROOT_PW
+fi
+
+if [ "$ROOT_PW" == "" ]; then
+	CMD=$@
+else
+	CMD="echo $ROOT_PW | sudo -S"
+
+	for token in $@; do
+		if [ "$token" == "$1" ]; then
+			continue
+		fi
+
+		CMD="$CMD"$' '"$token"
+	done
+fi
+
+echo "Sent cmd: [$CMD]"
+SERVER_LIST=`cat ${SCRIPTPATH}/server.list`
+for server in $SERVER_LIST; do
+	echo "=============== $server ==============="
+	ssh -t $server ${CMD}
+	echo -e "\n"
+done
