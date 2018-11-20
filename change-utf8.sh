968c11e1255c9691076cd4f89a39b9e2ec9c82b7
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2018-11-20 22:27:44 +0900

    change-utf8.sh: Remove variable IFS
    Remove variable IFS and continue to support whitespace of arguments.
    
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- change-utf8.sh
+++ change-utf8.sh
@@ -5,38 +5,27 @@ if [ $# == 0 ]; then
 	exit 1
 fi
 
-# Backup env IFS and then set IFS to newline
-IFS_BACKUP="$IFS"
-IFS=$'\n'
+FILE_NAME=$@
 
-FILE_NAME=$1
-
-for token in $@; do
-	if [ "$token" == "$1" ]; then
-		continue
-	fi
-	FILE_NAME="$FILE_NAME"$' '"$token"
-done
-
-CMD=`file $FILE_NAME | grep UTF | wc -l`
-USER=`ls -l $FILE_NAME | awk '{print $3}'`
-GROUP=`ls -l $FILE_NAME | awk '{print $4}'`
-
-if [ $CMD == 1 ]; then
-	echo "Already UTF-8"$'\t'"[$FILE_NAME]"
-	# Restore IFS
-	IFS=$IFS_BACKUP
+if ! [ -f "$FILE_NAME" ]; then
+	echo -e "This is a directory, only use file:\t[$FILE_NAME]"
 	exit 1
 fi
 
-touch ${FILE_NAME}_
+CMD=`file "$FILE_NAME" | grep UTF | wc -l`
+USER=`ls -l "$FILE_NAME" | awk '{print $3}'`
+GROUP=`ls -l "$FILE_NAME" | awk '{print $4}'`
 
-iconv -c -f euc-kr -t utf-8 $FILE_NAME > ${FILE_NAME}_
+if [ $CMD == 1 ]; then
+	echo -e "Already UTF-8\t[$FILE_NAME]"
+	exit 1
+fi
 
-rm -f $FILE_NAME
-mv ${FILE_NAME}_ $FILE_NAME
+touch "${FILE_NAME}_"
 
-sudo chown $USER:$GROUP $FILE_NAME
+iconv -c -f euc-kr -t utf-8 "$FILE_NAME" > "${FILE_NAME}_"
 
-# Restore IFS
-IFS=$IFS_BACKUP
+rm -f "$FILE_NAME"
+mv "${FILE_NAME}_" "$FILE_NAME"
+
+sudo chown $USER:$GROUP "$FILE_NAME"
