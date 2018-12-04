a888f922ace9e61ecec42d9b770d5d1301d0cc87
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2018-12-04 14:45:30 +0900

    lssize.sh: Add t option
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- lssize.sh
+++ lssize.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+help() {
+	echo "Usage: lssize.sh [OPTION]"
+	echo "Options:"
+	echo "  -t		sort by modification time, newest first"
+	echo "  -h		display this help"
+	exit 1
+}
+
 # Backup env IFS and then set IFS to newline
 IFS_BACKUP="$IFS"
 IFS=$'\n'
 
 ARR_LIST=(`ls -1`)
+while getopts "th" opt; do
+	case $opt in
+		t)
+			ARR_LIST=(`ls -1t`)
+			;;
+		h)
+			help
+			;;
+		*)
+			help
+			;;
+	esac
+done
 
 for list in ${ARR_LIST[@]}; do
 	du -sh "$list"
