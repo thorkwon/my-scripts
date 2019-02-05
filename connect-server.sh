f51ef79728414e905c51dc866641bd9257b7628c
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2019-02-06 00:19:38 +0900

    connect-server.sh: Add a check rule for server status
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- connect-server.sh
+++ connect-server.sh
@@ -19,13 +19,20 @@ elif [ -z "$1" ]; then
 	exit 1
 fi
 
+# check the server is online
+CMD=`nc -vz -w1 server 139 445 2>&1 | grep succeeded`
+if [ -z "$CMD" ]; then
+	echo "Please make sure the server is online."
+	exit 1
+fi
+
 # get uid and gid
 U_ID=`id | awk '{print $1}' | awk -F '(' '{print $1}'`
 G_ID=`id | awk '{print $2}' | awk -F '(' '{print $1}'`
 
 # You have to add hostname of server into '/etc/hosts'
 sudo mount -t cifs -o user=$1,$U_ID,$G_ID //server/Share /mnt/server
-CMD=$(sudo mount | grep server)
+CMD=`sudo mount | grep server`
 if [ -n "$CMD" ] ; then
 	echo "Mount server	user[$1]"
 fi
