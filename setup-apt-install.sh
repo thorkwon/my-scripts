a251860bf3454fb111575de4473ca14578debfa7
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2019-02-08 18:58:12 +0900

    setup-apt-install.sh: Fix apt usage style as an array
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- setup-apt-install.sh
+++ setup-apt-install.sh
@@ -7,18 +7,19 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-USED_GNOME_VER="18.00"
-RELEASE_VER=`lsb_release -r -s`
-if [ "`printf "%s\n" "$USED_GNOME_VER" "$RELEASE_VER" | sort -V | head -n1`" == "$USED_GNOME_VER" ]; then
-	echo "Release version $RELEASE_VER"
+CMD=`dpkg -l | grep gnome-control`
+if [ -n "$CMD" ]; then
+	echo "The gnome is used."
 	FLAG_USED_GNOME=1
-	sudo apt -y install gnome-tweak-tool
-	sudo apt -y install ubuntu-restricted-extras
-	sudo apt -y install net-tools
-	sudo apt -y install chrome-gnome-shell
-	sudo apt -y install x11-utils gnome-shell-extension-dashtodock
+	PKG_LISTS="
+	gnome-tweak-tool
+	ubuntu-restricted-extras
+	net-tools
+	chrome-gnome-shell
+	x11-utils gnome-shell-extension-dashtodock
+	"
+	sudo apt -y install $PKG_LISTS
 else
-	echo "Release version $RELEASE_VER"
 	FLAG_USED_GNOME=0
 fi
 
@@ -33,61 +34,67 @@ if [ "$1" == "base" -o "$1" == "all" ]; then
 	fi
 
 	echo "Install package for base"
-	sudo apt -y install neofetch
-	sudo apt -y install nmap
-	sudo apt -y install hping3
-	sudo apt -y install iperf3
-	sudo apt -y install qbittorrent
-	sudo apt -y install rdesktop
-	sudo apt -y install samba
-	sudo apt -y install sendemail
-	sudo apt -y install ssh
-	sudo apt -y install terminator
-	sudo apt -y install vim
-	sudo apt -y install vlc
-	sudo apt -y install xclip
-	sudo apt -y install git
-	sudo apt -y install git-email
-	sudo apt -y install tig
-	sudo apt -y install tree
-	sudo apt -y install ccache
-	sudo apt -y install cscope
-	sudo apt -y install ctags
-	sudo apt -y install gcp
-	sudo apt -y install cifs-utils
-	sudo apt -y install dkms
-	sudo apt -y install screen
-	sudo apt -y install curl
-	sudo apt -y install fcitx-hangul
-	sudo apt -y install aria2
+	PKG_LISTS="
+	neofetch
+	nmap
+	hping3
+	iperf3
+	qbittorrent
+	rdesktop
+	samba
+	sendemail
+	ssh
+	terminator
+	vim
+	vlc
+	xclip
+	git
+	git-email
+	tig
+	tree
+	ccache
+	cscope
+	ctags
+	gcp
+	cifs-utils
+	dkms
+	screen
+	curl
+	fcitx-hangul
+	aria2
 
-	sudo apt -y install cmake
-	sudo apt -y install autoconf
-	sudo apt -y install autotools-dev
-	sudo apt -y install gawk git-core
-	sudo apt -y install diffstat unzip texinfo gcc-multilib
-	sudo apt -y install build-essential chrpath socat libsdl1.2-dev
-	sudo apt -y install python3-pip
-	sudo apt -y install g++
-	sudo apt -y install gcc
+	cmake
+	autoconf
+	autotools-dev
+	gawk git-core
+	diffstat unzip texinfo gcc-multilib
+	build-essential chrpath socat libsdl1.2-dev
+	python3-pip
+	g++
+	gcc
+	"
+	sudo apt -y install $PKG_LISTS
 fi
 
 # coding and compiler
 if [ "$1" == "all" ]; then
 	echo "Install package for coding"
-	sudo apt -y install g++-arm-linux-gnueabi
-	sudo apt -y install gcc-arm-linux-gnueabi
-	sudo apt -y install device-tree-compiler
-	sudo apt -y install flex
-	sudo apt -y install bison
-	sudo apt -y install lthor
-	sudo apt -y install minicom
-	sudo apt -y install android-tools
-	sudo apt -y install android-tools-adb android-tools-fastboot
-	sudo apt -y install libqt4-dev libncurses5-dev
-	sudo apt -y install curlftpfs
-	sudo apt -y install emdebian-archive-keyring
-	sudo apt -y install fbterm
-	sudo apt -y install ia32-libs-multiarch
-	sudo apt -y install lzop
+	PKG_LISTS="
+	g++-arm-linux-gnueabi
+	gcc-arm-linux-gnueabi
+	device-tree-compiler
+	flex
+	bison
+	lthor
+	minicom
+	android-tools
+	android-tools-adb android-tools-fastboot
+	libqt4-dev libncurses5-dev
+	curlftpfs
+	emdebian-archive-keyring
+	fbterm
+	ia32-libs-multiarch
+	lzop
+	"
+	sudo apt -y install $PKG_LISTS
 fi
