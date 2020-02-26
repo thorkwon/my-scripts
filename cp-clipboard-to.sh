fea648898eb99460ad64ae6a6b2e0111a6637cd8
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2020-02-27 00:08:05 +0900

    cp-clipboard-to.sh: Add copy clipboard to dst
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ cp-clipboard-to.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# == 0 ]; then
+	echo "Usage: $0 <DST>"
+	exit 1
+fi
+
+DST="$1"
+
+MSG=`xclip -o`
+echo "Clipboard = [$MSG]"
+
+MSG="echo '${MSG}' >> clipboard"
+ssh -t ${DST} ${MSG}
