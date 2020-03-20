84110643af6b2328aadae0de9eb3d588412c3eb2
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2020-03-21 01:27:27 +0900

    uninstall_cmake.sh: Add error handling to check removal list
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- uninstall_cmake.sh
+++ uninstall_cmake.sh
@@ -3,15 +3,19 @@
 INSTALL_LIST="install_manifest.txt"
 
 CMD=`ls | grep install_manifest.txt`
-
-if [ -z $CMD ]; then
+if [ -z "$CMD" ]; then
 	echo "${INSTALL_LIST}: Not found"
 	exit 1
 fi
 
+CMD=`cat ${INSTALL_LIST} | xargs ls 2>&1 | grep "No such file or directory"`
+if ! [ -z "$CMD" ]; then
+	echo "Already removed..."
+	exit 0
+fi
+
 echo -e "=== Remove list ==="
 cat ${INSTALL_LIST}
-
-echo -e "\n=== Remove ==="
 cat ${INSTALL_LIST} | sudo xargs rm
-cat ${INSTALL_LIST} | xargs -L1 dirname | sudo xargs rmdir -p
+cat ${INSTALL_LIST} | xargs -L1 dirname | sudo xargs rmdir -p 2>/dev/null
+echo -e "\n=== All Removed ==="
