252ce52a7124b62a2c29d5b6c03d8137289cc7c1
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2020-04-14 22:42:47 +0900

    auto-translation2ko_srt.sh: Add auto script for translate python code
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ auto-translate2_srt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# English srt subtitles to Korean srt translation.
+
+if [ $# != 1 ]; then
+	echo "Usage: $0 <filename>"
+	exit 1
+fi
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+if [ -f "${1}.backup" ]; then
+	echo "Already translation Done..."
+	exit 0
+fi
+
+USER=`ls -l "$1" | awk '{print $3}'`
+GROUP=`ls -l "$1" | awk '{print $4}'`
+
+python3 ${SCRIPTPATH}/translate2ko_srt.py $1
+sudo chown $USER:$GROUP "$1"
