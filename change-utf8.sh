3d6affa11ead7a97982ab629a5d4a566b16aedf0
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2020-07-01 14:55:45 +0900

    change-utf8.sh: Fix rules for make change utf8 file
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- change-utf8.sh
+++ change-utf8.sh
@@ -13,19 +13,18 @@ if ! [ -f "$FILE_NAME" ]; then
 fi
 
 CMD=`file "$FILE_NAME" | grep UTF | wc -l`
-USER=`ls -l "$FILE_NAME" | awk '{print $3}'`
-GROUP=`ls -l "$FILE_NAME" | awk '{print $4}'`
-
 if [ $CMD == 1 ]; then
 	echo -e "Already UTF-8\t[$FILE_NAME]"
 	exit 1
 fi
 
 touch "${FILE_NAME}_"
+if ! [ -f "${FILE_NAME}_" ]; then
+	echo "touch: cannot touch '${FILE_NAME}_': Permission denied"
+	exit 1
+fi
 
 iconv -c -f euc-kr -t utf-8 "$FILE_NAME" > "${FILE_NAME}_"
+cat "${FILE_NAME}_" > "${FILE_NAME}"
 
-rm -f "$FILE_NAME"
-mv "${FILE_NAME}_" "$FILE_NAME"
-
-sudo chown $USER:$GROUP "$FILE_NAME"
+rm -f "${FILE_NAME}_"
