b1ae670fb59021ab8aa0e146def5c34310b27c54
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2020-11-24 16:28:07 +0900

    setup-initial-mac: Add setup initial mac script
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ setup-initial-mac.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+OS=`uname`
+if ! [ "$OS" == "Darwin" ]; then
+	exit 0
+fi
+
+STATUS=`sysctl -a | grep "debug.lowpri_throttle_enabled" | awk '{print $2}'`
+if ! [ $STATUS == 1 ]; then
+	echo "Already done setup inital mac"
+	exit 0
+fi
+
+sudo sysctl debug.lowpri_throttle_enabled=0
+sudo pfctl -evf /etc/pf.conf
